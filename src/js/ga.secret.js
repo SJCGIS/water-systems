a44a7db0ffb5a7b7353a43d7e340f067fe018e09
Nick Peihl <nickp@sanjuanco.com>
2015-06-10 09:17:59 -0700

    Add analytics code

---
+++ src/js/ga.secret.js
@@ -0,0 +1,11 @@
+// google-analytics
+// Change the UA ID in the code below to match your ID
+// Rename file to ga.js
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-xxxxx', 'auto');
+ga('send', 'pageview');
