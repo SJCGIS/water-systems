336019eb868fd73991f07b355aea4e329c1063ce
Nick Peihl <nickp@sanjuanco.com>
2015-06-11 09:02:43 -0700

    Make JSHint play nice with Google Analytics

--- src/js/ga.secret.js
+++ src/js/ga.secret.js
@@ -2,6 +2,8 @@
 // Change the UA ID in the code below to match your ID
 // Rename file to ga.js
 
+/* jshint ignore:start */
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,3 +11,5 @@
 
 ga('create', 'UA-xxxxx', 'auto');
 ga('send', 'pageview');
+
+/* jshint ignore:end */
